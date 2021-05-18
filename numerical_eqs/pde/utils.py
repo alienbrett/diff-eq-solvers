5a80bac4b88b906091e9a113e7be0060b5e5ac5e
Brett Graves <alienbrett648@gmail.com>
2021-05-18 00:29:49 +0000

    250% speedup on banded_jacobian_approx() in pde->utils

--- numerical_eqs/pde/utils.py
+++ numerical_eqs/pde/utils.py
@@ -58,28 +58,6 @@ def banded_jacobian_approx(f, x0, n_low, n_high, eps=1e-8, *args, **kwargs):
     Designed to minimize evaluations to the callable
     '''    
 
-    # # Tests for banded jacobian solve
-    # k = 10
-
-    # f0 = lambda x: tridiag_mult(
-    #     np.asarray([
-    #         np.arange(0, k),
-    #         np.ones(k),
-    #         k-1-np.arange(0, k),
-    #     ]),
-    #     (x - np.ones(k)),
-    # )
-    # x00 = np.zeros(k)
-    # f1 = lambda x: 4 * (x+1)
-    # x01 = np.zeros(6)
-    # banded_jacobian_approx(
-    #     f = f0,
-    #     x0 = x00,
-    #     n_low = 1,
-    #     n_high = 1,
-    #     eps = 0.03
-    # )
-
     # Some sizes
     n_x = x0.size
     k = n_low + n_high + 1
@@ -90,12 +68,9 @@ def banded_jacobian_approx(f, x0, n_low, n_high, eps=1e-8, *args, **kwargs):
     # Our eventual result
     A = np.zeros((k, n_x))
 
-    # This will make computation a tiny bit more readable hopefully?
-    xs = np.arange(0,n_x)
-    
     for i in range(k):
         # Where our eval points
-        s = slice(i, n_x, k)
+        s = np.arange(i, n_x, k)
         # Get our (x0 + eps)
         x1 = x0.copy()
         x1[s] += eps
@@ -104,17 +79,27 @@ def banded_jacobian_approx(f, x0, n_low, n_high, eps=1e-8, *args, **kwargs):
         # The numerical derivative
         d_x1 = (f_x1 - f_x0)/eps
         
-        # Take each island and aggregate into A
-        for j in xs[s]:
-            # Get the indeces of values we want
-            raw_index = np.arange(j-n_low, j+n_high+1)
-            index = np.mod( raw_index, n_x )
-            
-            d = np.take_along_axis(d_x1, index, 0)
-            
-            # Mask out values that aren't in the region we want
-            A[:, j] = np.where( raw_index * (n_x - raw_index - 1) < 0, 0, d )
+        # if > 0,
+        # we need to cut some numbers, and pad zeros on the back
+        # if < 0,
+        # We need to pad in front with zeros
+        offset = s[0] + n_high + 1
+        
+        # After this, each slice (axis 1) will correspond to the i'th (axis 1) of A
+        full_d = np.concatenate([
+            [0]*k,
+            d_x1,
+            [0]*k,
+        ])[offset : offset + k*s.size]
+        
+        
+        # Load these index values in
+        np.put_along_axis(
+            arr = A,
+            indices = np.expand_dims(s, 0),
+            values = full_d.reshape(s.size,k).T,
+            axis = 1,
+        )
             
     
     return A
-
